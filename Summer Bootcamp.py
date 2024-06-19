5ab6831acdb1a6aaedcc8b7b74bdd7cd0198c260
Aylaaila <aila.hoylar@aclcbutuan.edu.ph>
2024-06-18 23:03:38 -0700

    version 1

---
+++ Summer Bootcamp.py
@@ -0,0 +1,10 @@
+word = input("Word: ")
+print(word.title())
+print(word.capitalize().replace("p","P"))
+print(word[-8:-4].replace ("b","L"))
+print(word[11].replace("p","E"))
+print(word[-3].upper() + word[-10].upper())
+print(word.replace(" ", "ai").isalpha())
+
+
+
